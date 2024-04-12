d334d70429411827b22157b45cd1d32dbb1251bf
Devel User <alstavedahl@g.ucla.edu>
2024-04-12 20:38:17 +0000

    Content Length Works
    #What
    Skeleton for body parsing, and content-length implementation
    
    #Why
    Curl requests would hang forever, as the HTTP header I had set up previously didn't have a content-length header. I've implemented one to make the request complete.
    
    #How
    The body is now assigned to its own string, which we then take the size of for the header.
    
    Change-Id: Ib6e54651b2e861196f18f4f422105b5f63fe7066

--- src/session.cc
+++ src/session.cc
@@ -1,5 +1,7 @@
+#include <cstddef>
 #include <cstdlib>
 #include <iostream>
+#include <string>
 #include "session.h"
 
 
@@ -18,16 +20,24 @@ void session::start() {
             boost::asio::placeholders::bytes_transferred));
 }
 
+std::string parse_data(char* data){
+    //TODO: this currently assumes we recieved a valid line
+    //TODO: will implement in next commit
+    //std::stringstream ss(data);
+}
 
 void session::handle_read(const boost::system::error_code& error, size_t bytes_transferred) {
     std::cout << "Handle Read Data:" << std::endl;
     std::cout << data_ << std::endl;
     if (!error) {
         std::cout << "Sending Response..." << std::endl;
-        std::string test_data = "HTTP/1.1 200 OK\nContent-Type: text/plain\n\n";
-        test_data = test_data + data_;
+        std::string http_response = "HTTP/1.1 200 OK\nContent-Type: text/plain\n";
+        std::string content_length = "Content-Length: ";
+        std::string response_body = data_;
+        content_length = content_length + std::to_string(response_body.size()) + "\n\n";
+        http_response = http_response + content_length + response_body;
         boost::asio::async_write(socket_,
-            boost::asio::buffer(test_data, test_data.size()), 
+            boost::asio::buffer(http_response, http_response.size()), 
             boost::bind(&session::handle_write, 
                 this,
                 boost::asio::placeholders::error));
