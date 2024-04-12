e49e070401a1e85964cef90ebb15faaa0feebb64
Devel User <rachtruong@g.ucla.edu>
2024-04-12 04:49:37 +0000

    Refactored server_main.cc - Moved classes server and session into seperate source + header files. - Changed CMakeLists.txt to use new files
    Change-Id: I3e68adcf90be94acf21f5ddca7b67016c8e457c4

---
+++ include/server.h
@@ -0,0 +1,17 @@
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+#include "session.h"
+
+using boost::asio::ip::tcp;
+
+class server {
+public:
+    server(boost::asio::io_service& io_service, short port);
+
+private:
+    boost::asio::io_service& io_service_;
+    tcp::acceptor acceptor_;
+
+    void start_accept();
+    void handle_accept(session* new_session, const boost::system::error_code& error);
+};
---
+++ include/session.h
@@ -0,0 +1,19 @@
+#include <boost/bind.hpp>
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
+
+class session {
+public:
+    session(boost::asio::io_service& io_service);
+    tcp::socket& socket();
+    void start();
+
+private:
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+
+    void handle_read(const boost::system::error_code& error, size_t bytes_transferred); 
+    void handle_write(const boost::system::error_code& error);
+};
