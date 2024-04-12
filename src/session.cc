7119470b70a81a9a5d3bc425667153fdd63d026f
Devel User <alstavedahl@g.ucla.edu>
2024-04-12 23:09:52 +0000

    return body
    #What
    Updated code to return the body of a request. Can test with:
    `curl -X POST http://localhost:8080 -d "Hello World"`
    
    #Why
    We're supposed to be returning the body in the assignment guidelines.
    
    #How
    There's a data parsing function now that takes in the raw data and extracts the information.
    
    #TODO
    For some reason the response handler seems to be called twice, and errors out once each time - I do not know why as of now, but the code still works properly behavior-wise so I figured I should leave that to future commits.
    
    Additionally, the data parsing function should eventually check for proper requests.
    
    
    
    Change-Id: Ided3a36eb2247bf8f3351ea09688446037d2c7d8

--- src/session.cc
+++ src/session.cc
@@ -23,19 +23,38 @@ void session::start() {
 std::string parse_data(char* data){
     //TODO: this currently assumes we recieved a valid line
     //TODO: will implement in next commit
-    //std::stringstream ss(data);
+
+    //credit: https://stackoverflow.com/questions/13172158/c-split-string-by-line
+    std::stringstream ss(data);
+    std::string to;
+    std::string ret = "";
+    bool foundBody = false;
+    while(std::getline(ss, to,'\n')){
+        if(foundBody){
+            ret = ret + to + "\n";
+        }
+        if(!foundBody && to.size() == 1){ //not sure how to find body exactly
+            foundBody = true;
+        }
+    }
+    return ret;
 }
 
 void session::handle_read(const boost::system::error_code& error, size_t bytes_transferred) {
+    std::cout << "-------------" << std::endl;
     std::cout << "Handle Read Data:" << std::endl;
     std::cout << data_ << std::endl;
     if (!error) {
+        //log
         std::cout << "Sending Response..." << std::endl;
+        //generate response
         std::string http_response = "HTTP/1.1 200 OK\nContent-Type: text/plain\n";
         std::string content_length = "Content-Length: ";
-        std::string response_body = data_;
+        std::string response_body = parse_data(data_);
         content_length = content_length + std::to_string(response_body.size()) + "\n\n";
         http_response = http_response + content_length + response_body;
+        std::cout << http_response << std::endl;
+        //send response
         boost::asio::async_write(socket_,
             boost::asio::buffer(http_response, http_response.size()), 
             boost::bind(&session::handle_write, 
@@ -43,9 +62,10 @@ void session::handle_read(const boost::system::error_code& error, size_t bytes_t
                 boost::asio::placeholders::error));
     }
     else {
-        //std::cout << "Error in Handle Read" << std::endl;
+        std::cout << "Error in Handle Read" << std::endl;
         delete this;
     }
+    std::cout << "-------------" << std::endl;
 }
 
 
